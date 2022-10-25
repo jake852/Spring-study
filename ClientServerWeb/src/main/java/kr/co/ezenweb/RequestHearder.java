ac117a5b344181e915c6e48ec045ec9812e26d80
jake852 <jake852@daum.net>
2022-10-25 16:55:45 +0900

    2022.10.25

---
+++ ClientServerWeb/src/main/java/kr/co/ezenweb/RequestHearder.java
@@ -0,0 +1,26 @@
+package kr.co.ezenweb;
+
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RequestHearder {
+
+	@RequestMapping("/requestHearder")
+	public void ezen(HttpServletRequest request) {
+	
+		
+		Enumeration<String> e = request.getHeaderNames();
+	
+		while(e.hasMoreElements()) {
+			String name = e.nextElement();
+			System.out.println(name + " " + request.getHeader(name));
+		}
+		
+	}
+	
+}
---
+++ RequestMessageWeb/src/main/java/kr/co/ezenstaion/LoginController.java
@@ -0,0 +1,76 @@
+package kr.co.ezenstaion;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller("loginController")
+public class LoginController {
+
+	@RequestMapping(value = {"/req/loginForm.do", "/req/loginForm2.do"}, method = {RequestMethod.GET})
+	public ModelAndView loginForm() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("/requestMessage/loginform");
+		return mav;
+	}
+	
+	@RequestMapping(value = "/req/login.do", method = {RequestMethod.GET, RequestMethod.POST})
+	public ModelAndView login(HttpServletRequest request) {
+		ModelAndView mav = new ModelAndView();
+		
+		
+		// request line
+		String requestLine = request.getMethod();				//GET 또는 POST
+		requestLine += " " + request.getRequestURI();			// /ezenweb/requestmesssage
+		
+		String queryString = request.getQueryString();			//  year=2022&month=10&day=13
+		requestLine += queryString == null ? "" : "?" + queryString;
+		requestLine += "" + request.getProtocol(); 				// HTTP/1.1
+		System.out.println(requestLine);
+		
+		// request header 
+		Enumeration<String> e = request.getHeaderNames();
+	
+		while(e.hasMoreElements()) {
+			String name = e.nextElement();
+			System.out.println(name + " " + request.getHeader(name));
+
+		}
+		
+		// request body -- Post일 때만 해당, GET은 body가 없음
+		final int CONTENT_LENGTH = request.getContentLength();
+		System.out.println("content length : " + CONTENT_LENGTH);
+		
+		if (CONTENT_LENGTH > 0) {
+			try {
+				request.setCharacterEncoding("utf-8");
+				mav.setViewName("/requestMessage/result");
+				
+				String userId = request.getParameter("userID");
+				String userName = request.getParameter("u"
+						+ "serName");
+				
+				mav.addObject("userId", userId);
+				mav.addObject("userName", userName);
+				
+				System.out.println();
+				System.out.println("userId = " + userId + ", userName =" + userName);
+				
+		} catch (UnsupportedEncodingException e1) {e1.printStackTrace();}
+			
+		}
+		
+		return mav;
+	}
+	
+}
+
