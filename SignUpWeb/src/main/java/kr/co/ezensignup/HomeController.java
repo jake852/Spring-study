7f6bbb7e331b43893b20da67e43572114b162ba7
jake852 <109125472+jake852@users.noreply.github.com>
2023-02-20 18:38:21 +0900

    Update HomeController.java

--- SignUpWeb/src/main/java/kr/co/ezensignup/HomeController.java
+++ SignUpWeb/src/main/java/kr/co/ezensignup/HomeController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+
 /**
  * Handles requests for the application home page.
  */
@@ -22,7 +23,7 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "/", method = RequestMethod.GET)  /*받아오기*/
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
